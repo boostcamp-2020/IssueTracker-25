64d6c6f959200d703f3f1d8d3c55788914070705
rolled-potatoes <keep4404@gmail.com>
2020-10-26 22:48:17 +0900

    Chore : 불필요한 log 파일 제거 및 eslint 환경 node 추가

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
   parserOptions: {
