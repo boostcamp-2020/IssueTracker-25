2354c7d7f8421a0ebeea84de064b8fe1862e6a5f
rolled-potatoes <keep4404@gmail.com>
2020-10-26 20:40:54 +0900

    Style : eslint 설정 추가
    통합된 개발 환경 구축을 위해 eslint-airbnb 환경을 설정 함.

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-const App = () => {
-    const [title, settitle] = useState('hellowww');
-    function sss1() {
-        settitle("asd")
-    }
-    return (
-        <div>
-            <div>{title}</div>
-            <button onClick={sss1}> button</button>
-        </div>
 
-    )
-}
+const App = () => {
+  const [title, settitle] = useState('hellowww');
+  function sss1() {
+    settitle('asd');
+  }
+  return (
+    <div>
+      <div>{title}</div>
+      <button onClick={sss1}> button</button>
+    </div>
+  );
+};
 export default App;
