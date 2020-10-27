845ce302d69852bc0cfc25baa8242847a82605fc
rolled-potatoes <keep4404@gmail.com>
2020-10-27 15:37:16 +0900

    Chore : build 스크립트 추가 및 배포 파일 생성
    도커 이미지 생성을 위해서 Dockerfile을 생성 함.
    그리고 nginx 동작에 필요한 설정 파일을 생성 함.
    
    배포 시 webpack 빌드 명령어를 package.json에 추가 함.

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,9 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[hash].js',
   },
   devServer: {
