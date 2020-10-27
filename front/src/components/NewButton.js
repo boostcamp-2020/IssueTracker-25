b3e81bd06035ce9680b4a6e595c1320ac5617488
yejineee <21700431@handong.edu>
2020-10-27 17:48:50 +0900

    FEAT: props.children 사용 & 태그 이름 변화
    props.children을 사용하여 컴포넌트에서 다른 컴포넌트를 담을 수 있도록 하였다.
    태그 이름을 StyledButton에서 좀 더 구체적인 이름으로 변경하였다.

--- front/src/components/NewButton.js
+++ front/src/components/NewButton.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import color from './color';
 
-const StyledButton = styled.button`
+const StyledNewButton = styled.button`
   width: fit-content;
   height: 2.5rem;
   border-radius: 0.4rem;
 
-  background: #2da44f;
-  color: white;
+  background: ${color.green};
 
+  color: white;
   font-size: 1rem;
 `;
 
-function NewButton(props) {
-  const { title, handleClick } = props;
-  return <StyledButton onClick={handleClick}>{title}</StyledButton>;
+function NewButton({ handleClick, children }) {
+  return <StyledNewButton onClick={handleClick}>{children}</StyledNewButton>;
 }
 
 NewButton.propTypes = {
-  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
   handleClick: PropTypes.func.isRequired,
 };
 
