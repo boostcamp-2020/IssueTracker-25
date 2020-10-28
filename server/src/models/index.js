f2a2d28ecaf320d7ae6739de1d40f7fe681c76f3
Changehee Choi <changhee.dev@gmail.com>
2020-10-28 23:00:18 +0900

    Feat: Milestone Entity 구현

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,6 +3,7 @@ import config from '../config';
 import User from './user';
 import Issue from './issue';
 import Label from './label';
+import Milestone from './milestone';
 
 const sequelizeConfigs = {
   ...config.db,
@@ -20,5 +21,6 @@ const sequelize = new Sequelize(sequelizeConfigs);
 User.init(sequelize);
 Issue.init(sequelize);
 Label.init(sequelize);
+Milestone.init(sequelize);
 
 export default { Sequelize, sequelize };
