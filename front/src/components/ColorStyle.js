2d7e2dbcc2b4d057b7b12e645500f25f00593d94
이수경 <dltnrud987@gmail.com>
2020-10-29 00:32:56 +0900

    Feat : colorStyle 추가
    button 테마색을 미리 지정하기 위해서 colorStyle.js 추가.

---
+++ front/src/components/ColorStyle.js
@@ -0,0 +1,23 @@
+import OpenColor from 'open-color';
+// https://yeun.github.io/open-color/
+
+const colors = {
+  default: {
+    background: OpenColor.white,
+    color: OpenColor.gray[9],
+  },
+  green: {
+    background: OpenColor.green[7],
+    color: OpenColor.white,
+  },
+  gray: {
+    background: OpenColor.gray[6],
+    color: OpenColor.white,
+  },
+  whiteRed: {
+    background: OpenColor.white,
+    color: OpenColor.red[8],
+  },
+};
+
+export default colors;
