45c87dbf44ffcba9590a27cac3a17977cbfcba87
Changehee Choi <changhee.dev@gmail.com>
2020-10-28 21:56:46 +0900

    Feat: Sequelize 연결 설정

---
+++ server/src/models/index.js
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize';
+import config from '../config';
+
+const sequelizeConfigs = {
+  ...config.db,
+  define: {
+    engine: 'InnoDB',
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_unicode_ci',
+    underscored: true,
+  },
+  logging: process.env.NODE_ENV === 'test' ? false : console.log,
+};
+
+const sequelize = new Sequelize(sequelizeConfigs);
+
+export default { Sequelize, sequelize };
