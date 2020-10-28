24e308339bcaa8fadaffccace300b90cd4fcce3e
yejineee <21700431@handong.edu>
2020-10-29 03:51:00 +0900

    Feat: IssueListPage에서 api요청하여 데이터 받는 것 구현
    IssueListPage에서 api요청으로 issue들을 받아서, 자식 컴포넌트에게
    전달하여 렌더링하도록 해야 한다.
    따라서 api를 요청한 후, issue들을 받고, 상태 변경을 useReducer를 사용하도록 하였다.

--- front/src/routes/IssueList/IssueListPage.js
+++ front/src/routes/IssueList/IssueListPage.js
@@ -1,20 +1,49 @@
-import React, { useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import issueApi from '../../apis/issue';
 import IssueListContainer from './IssueListContainer';
+import reducer from './issueHook';
+import actionType from './issueAction';
+
+const { FETCH_SUCCESS, FETCH_ERROR } = actionType;
 
 function IssueListPage() {
-  const [checkedIssues, setCheckedIssues] = useState({
-    issues: issueItems.map((issue) => {
-      return { ...issue, checked: false };
-    }),
+  const [state, dispatch] = useReducer(reducer, {
+    issues: undefined,
     allIssue: false,
+    error: undefined,
+    loading: true,
   });
 
+  const fetchIssue = async () => {
+    try {
+      const response = await issueApi.getIssues();
+      dispatch({ type: FETCH_SUCCESS, issueItems: response });
+    } catch (e) {
+      dispatch({ type: FETCH_ERROR, error: e.message });
+    }
+  };
+
+  useEffect(() => {
+    fetchIssue();
+  }, []);
+
+  const { issues, allIssue, error, loading } = state;
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (loading) {
+    return null;
+  }
+  if (state.issues && state.issues.length === 0) {
+    return <div>No results matched your search.</div>;
+  }
   return (
     <>
       <IssueListContainer
-        checkedIssues={checkedIssues}
-        setCheckedIssues={setCheckedIssues}
-      ></IssueListContainer>
+        issues={issues}
+        allIssue={allIssue}
+        dispatch={dispatch}
+      />
     </>
   );
 }
