6eb325963a8727603e48c33900bfd474b15ab602
yejineee <21700431@handong.edu>
2020-10-29 03:21:48 +0900

    Chore: issue의 useReducer에서 사용되는 action type 상수 저장
    useReducer에서 사용되는 action의 type을 상수로 지정하여,
    코드 오류를 줄이고, 생산성을 높이도록 하였다.

---
+++ front/src/routes/IssueList/issueAction.js
@@ -0,0 +1,6 @@
+export default {
+  FETCH_SUCCESS: 'fetch_success',
+  FETCH_ERROR: 'fetch_error',
+  CHECK_ISSUE_HANDLER: 'check_issue_handler',
+  CHECK_ALL_ISSUE_HANDLER: 'check_all_issue_handler',
+};
