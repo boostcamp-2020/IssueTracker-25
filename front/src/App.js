12421f6b2649c11b5a8d71756005a4ebae50c388
yejineee <21700431@handong.edu>
2020-10-30 11:39:21 +0900

    Style: font 지정 및 바디의 margin 없앰

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import IssueListPage from './routes/IssueList/IssueListPage';
 import Header from './components/Header';
+
+const GlobalStyled = createGlobalStyle`
+  @font-face {
+    font-family: 'IBMPlexSansKR-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  body{
+    margin : 0 ; 
+
+    font-family:'IBMPlexSansKR-Regular';
+  }
+`;
 
 const App = () => {
   return (
     <>
+      <GlobalStyled />
       <Header />
       <IssueListPage />
     </>
