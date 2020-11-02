0fb073d7735b79fc2d96b87a85be6dcb8b1ec9b2
이수경 <dltnrud987@gmail.com>
2020-11-02 15:33:47 +0900

    Refactor: 드롭다운 버튼 수정
    버튼 모양에 상관없이 드롭다운을 구현하도록 버튼 부분 수정.

--- front/src/components/DropDown.js
+++ front/src/components/DropDown.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import color from './color';
+import useOutSideClick from './useOutsideClick';
 
 const DropDownContainer = styled.div`
   display: inline-flex;
-  flex: fit-content;
   position: relative;
 `;
-const DropDownButton = styled.button`
-  flex-basis: fit-content;
-  background-color: transparent;
+const DropdownButton = styled.button`
+  background: transparent;
   border: none;
-  &:hover {
-    cursor: pointer;
-  }
 `;
 const DropDownMenu = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 1.3rem;
+  margin-top: 1.8rem;
   position: absolute;
   border: 1px solid ${color.boldBlue};
   border-radius: 0.2rem;
   background-color: white;
   z-index: 1;
 `;
+const DropDownBody = styled.div`
+  width: 100%;
+`;
 const Header = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   padding: 0.3rem;
   font-weight: bolder;
 
@@ -35,65 +34,56 @@ const Header = styled.div`
     min-width: max-content;
   }
 `;
-const HeaderSubContianer = styled.div`
-  display: flex;
-  felx-direction: row;
-`;
-const DropDownBody = styled.div`
-  width: 100%;
-`;
+// close button
 const CloseButton = styled.div`
   width: fit-content;
-  margin-left: 30px;
-  color: grey;
+  margin-left: 4rem;
   &: hover {
     cursor: pointer;
-    color: black;
   }
 `;
-const Icon = styled.img`
-  width: 0.5rem;
-  height: 0.5rem;
-`;
-const DropDown = ({ title, filter, children }) => {
+
+const DropDown = ({ headerText, button, children }) => {
   const [showMenu, setShowMenu] = useState(false);
+  const ref = useRef();
 
   const toggleHandler = () => {
     setShowMenu(!showMenu);
   };
+  const closeHandler = () => {
+    setShowMenu(false);
+  };
+  useOutSideClick(ref, closeHandler);
+
   return (
-    <DropDownContainer>
-      <DropDownButton onClick={toggleHandler}>
-        {title}
-        <Icon src="https://img.icons8.com/material-rounded/24/000000/give-way.png" />
-      </DropDownButton>
+    <DropDownContainer ref={ref}>
+      <DropdownButton className="dropdown-button" onClick={toggleHandler}>
+        {button}
+      </DropdownButton>
       {showMenu ? (
-        <DropDownMenu>
+        <DropDownMenu className="dropdown-menu">
           <DropDownHeader
-            filter={filter}
-            title={title}
+            headerText={headerText}
             toggleHandler={toggleHandler}
           />
-          <DropDownBody>{children}</DropDownBody>
+          <DropDownBody className="dropdown-body">{children}</DropDownBody>
         </DropDownMenu>
       ) : null}
     </DropDownContainer>
   );
 };
 
-const DropDownHeader = ({ filter = false, title, toggleHandler }) => {
-  const headerTitle = `Filter by ${title}`;
+const DropDownHeader = ({ headerText, toggleHandler }) => {
   return (
     <Header>
-      <HeaderSubContianer>
-        <div className="header-title">{headerTitle}</div>
-        <CloseButton onClick={toggleHandler}>
-          <Icon src="https://img.icons8.com/material-rounded/24/000000/delete-sign.png" />
-        </CloseButton>
-      </HeaderSubContianer>
-      <HeaderSubContianer>
-        {filter ? <input type="text" /> : null}
-      </HeaderSubContianer>
+      <div className="dropdown-header">{headerText}</div>
+      <CloseButton className="dropdown-button-close" onClick={toggleHandler}>
+        <img
+          className="button-image"
+          src="https://img.icons8.com/material-rounded/24/000000/delete-sign.png"
+          alt="close"
+        />
+      </CloseButton>
     </Header>
   );
 };
