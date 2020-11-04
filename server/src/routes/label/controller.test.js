385217540dffa8700f26c1fbe3b364b3f1f22711
Changhee Choi <changhee.dev@gmail.com>
2020-11-04 21:37:19 +0900

    Fix: Label API 관련 수정사항 반영 (#83)
    * Fix: Label API 관련 수정사항 반영
    
    - label 디렉토리 아래에서 label-controller.js 처럼 중복적으로 label
    을 붙이지 않는 방식으로 수정했습니다.
    
    - model을 import할때 model 모듈을 다이렉트로 import 하게 되면 사용방법에
    따라서 sequelize가 초기화 되지 않은 상태일 수도 있기 때문에 models/index.js를
    import해서 sequeilze의 models를 통해 model을 참조하는 방식으로 통일시켰습니다.
    
    * Fix: 누락된 파일 추가, __test__ 디렉토리 삭제
    
    테스트 코드는 해당 모듈과 같은위치에 생성하기로 결정함에 따라 기존
    테스트 코드를 모으기 위해 생성했던 디렉토리를 삭제했습니다.
    
    앞의 커밋에서 누락된 파일 추가했습니다.

--- server/src/routes/label/controller.test.js
+++ server/src/routes/label/controller.test.js
@@ -1,5 +1,5 @@
 import LabelService from '../../services/label';
-import LabelController from './label-controller';
+import LabelController from './controller';
 
 test('readList를 호출하였을 경우 응답 코드 200으로 list형태의 json을 반환한다.', async () => {
   const desiredLabelList = [{ id: 1 }, { id: 2 }];
