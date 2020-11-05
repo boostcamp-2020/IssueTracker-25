8a4ae4fdc243a48b13ec098fcb0fa86dd0f633e8
rolled-potatoes <keep4404@gmail.com>
2020-11-05 09:30:42 +0900

    Style : 컴포넌트 오타 수정

--- front/src/components/dropdown/LabelDropdownItem.js
+++ front/src/components/dropdown/LabelDropdownItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import DropdownItemContainer from './DropdownItemContainer';
-import CheckIconCompoent from '../CheckIconComponent';
+import CheckIconComponent from '../CheckIconComponent';
 import color from '../../libs/color';
 
 const LabelDropdownItemContainer = styled(DropdownItemContainer)`
@@ -31,7 +31,7 @@ const LabelDropdownItem = ({ label }) => {
   return (
     <LabelDropdownItemContainer>
       <div className="dropdown-item dropdown-item-check dropdown-item-top">
-        <CheckIconCompoent />
+        <CheckIconComponent />
       </div>
 
       <div className="dropdown-item dropdown-item-top">
--- front/src/components/dropdown/UserDropdownItem.js
+++ front/src/components/dropdown/UserDropdownItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import DropdownItemContainer from './DropdownItemContainer';
-import CheckIconCompoent from '../CheckIconComponent';
+import CheckIconComponent from '../CheckIconComponent';
 
 const UserDropdownItemContainer = styled(DropdownItemContainer)`
   .dropdown-item-check {
@@ -18,7 +18,7 @@ const UserDropdownItem = ({ userInfo }) => {
   return (
     <UserDropdownItemContainer>
       <div className="dropdown-item dropdown-item-check">
-        <CheckIconCompoent />
+        <CheckIconComponent />
       </div>
       <div className="dropdown-item">
         <img src={profileLink} alt="profile" />
