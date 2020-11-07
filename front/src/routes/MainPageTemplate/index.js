31c0cc4f9569bb14697f74c5bdf319714b07adf0
Changehee Choi <changhee.dev@gmail.com>
2020-11-07 21:55:52 +0900

    Refactor: MainPageTemplate 수정
    MainPageTemplate에서 공통 margin 속성을 처리하기 위해 main이란 태그를
    사용했었는데 브라우저상에서 React does not recognize the `computedMatch`
    prop on a DOM element 오류메세지를 띄우고 있었습니다. 일단 main 태그를
    사용하는 대신 MainContainer 컴포넌트를 만들어서 바꿔주었습니다.
    
    추가로 글로벌 style에서 body 태그의 css 내부에 설정할 필요가 없는 부분을
    분리 시켜주었습니다.

--- front/src/routes/MainPageTemplate/index.js
+++ front/src/routes/MainPageTemplate/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from '../../components/Header';
 import UserStore from '../user-store';
 import IssueListPage from '../IssueList/IssueListPage';
 import IssueDetailPage from '../IssueDetail/IssueDetailPage';
+
+const MainContainer = styled.div`
+  margin: 100px 200px;
+`;
 
 const MainPageTemplate = () => {
   return (
@@ -11,10 +16,10 @@ const MainPageTemplate = () => {
       <Header />
       <UserStore />
       <Switch>
-        <main>
+        <MainContainer>
           <Route exact path="/issues/:id" component={IssueDetailPage} />
           <Route exact path="/" component={IssueListPage} />
-        </main>
+        </MainContainer>
       </Switch>
     </>
   );
