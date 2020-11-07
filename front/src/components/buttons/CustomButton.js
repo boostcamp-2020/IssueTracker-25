5b021447e5e90d07f500f52f3eafa3855204ecc3
Changehee Choi <changhee.dev@gmail.com>
2020-11-07 22:04:28 +0900

    Refactor: CustomButton 컴포넌트 propTypes 제거
    CustomButton 컴포넌트의 propTypes를 제거했습니다.

--- front/src/components/buttons/CustomButton.js
+++ front/src/components/buttons/CustomButton.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import buttonTheme from './theme';
 
 const Button = styled.button`
@@ -21,9 +20,4 @@ function CustomButton({ handleClick, children, style = {} }) {
   );
 }
 
-CustomButton.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
-  style: PropTypes.objectOf(PropTypes.object).isRequired,
-};
 export default CustomButton;
