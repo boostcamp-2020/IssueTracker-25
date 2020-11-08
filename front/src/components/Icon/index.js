7d0126c63fe2f717cea073f0c5d8a23d07c7d814
이수경 <dltnrud987@gmail.com>
2020-11-09 04:29:00 +0900

    Feat: 톱니바퀴 아이콘 추가
    사용할 톱니바퀴아이콘컴포넌트 추가.

--- front/src/components/Icon/index.js
+++ front/src/components/Icon/index.js
@@ -1,3 +1,4 @@
 export { default as OpenIconComponent } from './OpenIconComponent';
 export { default as CheckIconComponent } from './CheckIconComponent';
 export { default as ClosedIconComponent } from './ClosedIconComponent';
+export { default as GearIconComponent } from './GearIconComponent';
