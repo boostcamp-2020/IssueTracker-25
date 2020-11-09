cb111b4fcdcd5e5df293bdbabf6ef947fdd151e4
이수경 <dltnrud987@gmail.com>
2020-11-09 23:30:16 +0900

    Feat: milestone service 로직 수정
    milestone api가 closed, open된 이슈의 개수 데이터를 가지도록 변경.

--- server/src/routes/milestone/index.js
+++ server/src/routes/milestone/index.js
@@ -3,11 +3,12 @@ import MilestoneController from './controller';
 import MilestoneService from '../../services/milestone';
 import db from '../../models';
 
-const { Milestone: MilestoneModel } = db.sequelize.models;
+const { sequelize, Sequelize } = db;
+const { Milestone: MilestoneModel, Issue: IssueModel } = db.sequelize.models;
 
 const router = express.Router();
 const milestoneController = MilestoneController(
-  MilestoneService({ MilestoneModel }),
+  MilestoneService({ MilestoneModel, IssueModel, sequelize, Sequelize }),
 );
 router.get('/', milestoneController.getMilestoneList);
 
--- server/src/services/milestone.js
+++ server/src/services/milestone.js
@@ -1,7 +1,32 @@
-const MilestoneService = ({ MilestoneModel }) => ({
-  getMilestoneList() {
-    return MilestoneModel.findAll();
-  },
-});
+const MilestoneService = ({
+  MilestoneModel,
+  IssueModel,
+  sequelize,
+  Sequelize,
+}) => {
+  const countOfIssueByMilestone = async () => {
+    const count = await IssueModel.findAll({
+      attributes: [
+        'milestoneId',
+        'isClosed',
+        [sequelize.fn('COUNT', sequelize.col('id')), 'count'],
+      ],
+      where: {
+        milestoneId: {
+          [Sequelize.Op.not]: null,
+        },
+      },
+      group: ['milestoneId', 'isClosed'],
+    });
+    return count;
+  };
+  const getMilestoneList = async () => {
+    const milestones = await MilestoneModel.findAll();
+    const counts = await countOfIssueByMilestone();
+    return { milestones, counts };
+  };
+
+  return { getMilestoneList };
+};
 
 export default MilestoneService;
