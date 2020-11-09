7e035ca560a67b3750dc80b89289c4cdcea8e47d
Changehee Choi <changhee.dev@gmail.com>
2020-11-09 23:48:08 +0900

    Feat: 글자수 표시 기능 구현
    내용을 입력하고 2초동안 타이핑을 하지 않으면 글자수를 표시하는 기능을
    구현했습니다.

--- front/src/components/MarkdownEditor/index.js
+++ front/src/components/MarkdownEditor/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import CustomTextarea from './CustomTextarea';
 import UploadInput from './UploadInput';
+import ContentsLengthLayer from './ContentsLengthLayer';
 import color from '../../libs/color';
+
+const DELAY = 2000;
 
 const EditorContainer = styled.div`
   width: 100%;
@@ -21,15 +24,44 @@ const EditorInputContainer = styled.div`
   }
 `;
 
+let debounceTimer;
+
 const MarkdownEditor = ({ onChange }) => {
+  const [contentsLength, setContentsLength] = useState(0);
+  const [showContentsLength, setShowContentsLength] = useState(false);
+
+  const setHideContentsLengthTimer = () => {
+    setTimeout(() => {
+      setShowContentsLength(false);
+    }, DELAY);
+  };
+
+  const setShowContentsLengthDebounce = (newContentsLength) => {
+    if (debounceTimer) {
+      clearTimeout(debounceTimer);
+    }
+    debounceTimer = setTimeout(() => {
+      setShowContentsLength(true);
+      setContentsLength(newContentsLength);
+      setHideContentsLengthTimer();
+    }, DELAY);
+  };
+
   const updateContents = (newContents) => {
     onChange(newContents);
+    setShowContentsLengthDebounce(newContents.length);
   };
 
   return (
     <EditorContainer>
       <EditorInputContainer>
         <CustomTextarea onChange={updateContents} />
+        {showContentsLength && (
+          <ContentsLengthLayer
+            className="markdown-editor__content-length"
+            length={contentsLength}
+          />
+        )}
       </EditorInputContainer>
       <UploadInput />
     </EditorContainer>
