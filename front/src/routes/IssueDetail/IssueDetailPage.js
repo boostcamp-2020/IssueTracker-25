4d083cee423e7a1d370ba3218a42754d41624599
Changhee Choi <changhee.dev@gmail.com>
2020-11-09 15:02:54 +0900

    Fix: 이슈 디테일 페이지 무한 렌더링 버그 수정 (#113)
    * Refactor: MainPageTemplate 수정
    
    MainPageTemplate에서 공통 margin 속성을 처리하기 위해 main이란 태그를
    사용했었는데 브라우저상에서 React does not recognize the `computedMatch`
    prop on a DOM element 오류메세지를 띄우고 있었습니다. 일단 main 태그를
    사용하는 대신 MainContainer 컴포넌트를 만들어서 바꿔주었습니다.
    
    추가로 글로벌 style에서 body 태그의 css 내부에 설정할 필요가 없는 부분을
    분리 시켜주었습니다.
    
    * Refactor: CustomButton 컴포넌트 propTypes 제거
    
    CustomButton 컴포넌트의 propTypes를 제거했습니다.
    
    * Refactor: 메인 레이아웃의 padding 값 조절
    
    현재 설정된 메인 레이아웃의 상하좌우 여백이 너무 넓게 설정된 느낌이
    있어서 GitHub 페이지와 비슷한 여백의 사이즈로 수정했습니다.
    
    * Refactor: 이슈 디테일 페이지 Header 컴포넌트 분리
    
    이슈 디테일 페이지의 Header 컴포넌트를 분리했습니다.
    
    * Refactor: 이슈 디테일 페이지 Body 컴포넌트 분리
    
    이슈 디테일 페이지의 Body 컴포넌트를 분리하고 반응형 처리를 추가했
    습니다.
    
    * Fix: 이슈 디테일 페이지 무한렌더링 이슈 해결
    
    이슈 디테일 페이지에서 발생했던 무한 렌더링 이슈를 해결하고 분리한
    Header, Body 컴포넌트를 적용시켰습니다.
    
    * Refactor: generateIssueTitle 함수명 변경
    
    Issue의 타이틀이 아닌 info를 생성하는 함수이므로 generateIssueInfo로
    함수 이름을 변경함.
    
    * Refactor: IssueDetailBody 컴포넌트의 css에서!important 제거
    
    불필요하게 사용된 !important 제거함.
    
    * Style: 컴포넌트 코드 구조를 일관성을 갖도록 수정
    
    이번에 작업하면서 함수 선언식과 함수 표현식을 섞어서 컴포넌트를
    만들었는데 기존 코드들과 일관되도록 함수 표현식으로 수정함.

--- front/src/routes/IssueDetail/IssueDetailPage.js
+++ front/src/routes/IssueDetail/IssueDetailPage.js
@@ -1,247 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
-import CustomButton from '../../components/buttons/CustomButton';
-import color from '../../libs/color';
-import { UserProfile } from '../../components/UserProfile';
-import IssueStateComponent from './IssueStateComponent';
+import IssueDetailHeader from './header/IssueDetailHeader';
+import IssueDetailBody from './body/IssueDetailBody';
 import issueAPI from '../../apis/issue';
+import utils from '../../libs/utils';
 
-const IssueDetailContainer = styled.div`
-  display: grid;
-  grid-auto-rows: minmax(120px, auto);
-  grid-template-columns: 2fr 1fr;
-  .issue-detail-header {
-    grid-column: 1 / 3;
-    grid-row: 1;
-    display: inline-block;
-    vertical-align: middle;
-
-    &__title {
-      font-size: 2rem;
-      font-weight: 900;
-    }
-
-    &__button {
-      margin-left: auto;
-      & button {
-        margin-left: 0.3rem;
-      }
-    }
-    &__divider {
-      margin: 1rem 0;
-      border: 1px solid ${color.lightGray};
-    }
-  }
-
-  .issue-detail-subheader {
-    display: flex;
-    align-items: center;
-    .state-icon {
-      color: ${color.white};
-      display: flex;
-      border-radius: 20rem;
-      justify-content: center;
-      align-items: center;
-      padding: 0.4rem 0.6rem;
-      &__content {
-        text-indent: 0.5rem;
-      }
-    }
-    &--closed {
-      background: ${color.red};
-    }
-    &--open {
-      background: ${color.green};
-    }
-  }
-
-  .button-text {
-    padding: 0 0.2rem;
-  }
-  .state-message {
-    text-indent: 0.5rem;
-  }
-  .issue-detail-aside {
-    grid-column: 2;
-    grid-row: 2;
-  }
-
-  .issue-detail-content {
-    grid-column: 1;
-    grid-row: 2;
-  }
-`;
-const CommentContainer = styled.div`
-  & + & {
-    margin-top: 2rem;
-  }
-  display: flex;
-  z-index: 1;
-  .comment-profile {
-    display: flex;
-    justify: center;
-  }
-  .comment-container {
-    position: relative;
-    &::before {
-      width: 1rem;
-      z-index: -1;
-      height: 1rem;
-      content: '';
-      background: ${color.lightGray};
-      position: absolute;
-      transform: rotate(45deg);
-      left: -8px;
-      top: 9px;
-    }
-    flex: 1;
-    margin: 0 1.5rem;
-    border: 1px solid ${color.lightGray};
-    > div {
-      padding: 0.5rem;
-    }
-  }
-  .comment-header {
-    display: flex;
-    background: ${color.lightGray};
-  }
-  .comment-writer {
-    padding-right: 0.3rem;
-    font-weight: bolder;
-  }
-  .owner-container {
-    display: flex;
-    margin-left: auto;
-
-    .comment-owner {
-      margin: 0 0.5rem;
-      padding: 0 0.2rem;
-      border: 1px solid ${color.lightBlue};
-    }
-  }
-  .edit-button {
-    border: none;
-    outline: 0;
-    background: ${color.lightGray};
-  }
-`;
-const initIssue = {
-  id: 0,
-  title: '',
-  contents: '',
-  isClosed: true,
-  closedAt: '',
-  authorId: 0,
-  isAuthor: true,
-  milestoneId: 0,
-  createdAt: '',
-  updatedAt: '',
-  Author: {
-    id: 0,
-    name: '',
-    uid: '',
-    profileLink: '',
-    createdAt: '',
-    updatedAt: '',
-  },
-  Labels: [],
-  Assignees: [],
-  Milestone: {
-    id: 0,
-    title: '',
-    description: '',
-    endDate: '',
-    createdAt: '',
-    updatedAt: '',
-  },
-  Comments: [],
-};
 const IssueDetailPage = () => {
-  const [issue, setIssue] = useState(initIssue);
+  const [issue, setIssue] = useState({});
   const { id } = useParams();
-  const {
-    title,
-    Author,
-    isClosed,
-    createdAt,
-    closedAt,
-    contents,
-    Comments,
-    isAuthor,
-  } = issue;
-  const countComment = Comments.length;
-  const issueState = `${Author.name} ${
-    isClosed ? 'closed' : 'opend'
-  } this issue ${isClosed ? closedAt : createdAt} ㆍ ${
-    countComment || 0
-  } comments`;
-  const getIssue = async () => {
-    const response = await issueAPI.getIssue(id);
-    setIssue(response);
-  };
-  useEffect(() => {
-    getIssue();
-  }, [issue]);
-  return (
-    <IssueDetailContainer>
-      <div className="issue-detail-header">
-        <div className="issue-detail-subheader">
-          <div className="issue-detail-header__title">{title}</div>
-          <div className="issue-detail-header__button">
-            <CustomButton style={{ color: 'default' }}>Edit</CustomButton>
-            <CustomButton style={{ color: 'green' }}>New issue</CustomButton>
-          </div>
-        </div>
-        <IssueStateComponent isClosed={isClosed} issueState={issueState} />
-        <div className="issue-detail-header__divider" />
-      </div>
-      <div className="issue-detail-content">
-        <Comment
-          writer={Author}
-          createdAt={createdAt}
-          contents={contents}
-          isAuthor={isAuthor}
-        />
-        {Comments.map((comment) => {
-          return (
-            <Comment
-              writer={comment.User}
-              createdAt={comment.createdAt}
-              contents={comment.contents}
-            />
-          );
-        })}
-      </div>
-      <div className="issue-detail-aside" />
-    </IssueDetailContainer>
-  );
-};
 
-const OWNER = 'Owner';
-const Comment = ({ writer, createdAt, contents, isAuthor }) => {
-  const commented = `commented ${createdAt}`;
+  const updateIssue = async () => {
+    const issueResponse = await issueAPI.getIssue(id);
+    const newIssueState = { ...issueResponse };
+    const countOfComments = issueResponse.Comments.length;
+    newIssueState.countOfComments = countOfComments;
+    setIssue(newIssueState);
+  };
+
+  useEffect(() => {
+    updateIssue();
+  }, []);
+
   return (
-    <CommentContainer>
-      <div className="comment-profile">
-        <UserProfile className="profile-container" src={writer.profileLink} />
-      </div>
-      <div className="comment-container">
-        <div className="comment-header">
-          <div className="comment-writer">{writer.name}</div>
-          <div>{commented}</div>
-          {isAuthor && (
-            <div className="owner-container">
-              <div className="comment-owner">{OWNER}</div>
-              <button type="button" className="edit-button">
-                Edit
-              </button>
-            </div>
-          )}
-        </div>
-        <div className="comment-content">{contents}</div>
-      </div>
-    </CommentContainer>
+    !utils.isEmpty(issue) && (
+      <>
+        <IssueDetailHeader issue={issue} />
+        <IssueDetailBody issue={issue} />
+      </>
+    )
   );
 };
 
---
+++ front/src/routes/IssueDetail/header/IssueDetailHeader.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import color from '../../../libs/color';
+import CustomButton from '../../../components/buttons/CustomButton';
+import IssueDetailHeaderInfo from './IssueDetailHeaderInfo';
+
+const IssueDetailHeaderContainer = styled.div`
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid ${color.lightGray};
+
+  .issue-detail-header {
+    &__title-container {
+      display: flex;
+      vertical-align: middle;
+      margin-bottom: 0.5rem;
+    }
+
+    &__title {
+      flex: 1;
+      font-size: 2rem;
+      font-weight: 900;
+    }
+
+    &__buttons {
+      flex: 0 0 auto;
+      button + button {
+        margin-left: 0.3rem;
+      }
+    }
+  }
+`;
+
+const IssueDetailHeader = ({ issue }) => {
+  return (
+    <IssueDetailHeaderContainer>
+      <div className="issue-detail-header__title-container">
+        <div className="issue-detail-header__title">{issue.title}</div>
+        <div className="issue-detail-header__buttons">
+          <CustomButton style={{ color: 'default' }}>Edit</CustomButton>
+          <CustomButton style={{ color: 'green' }}>New issue</CustomButton>
+        </div>
+      </div>
+      <IssueDetailHeaderInfo issue={issue} />
+    </IssueDetailHeaderContainer>
+  );
+};
+
+export default IssueDetailHeader;
