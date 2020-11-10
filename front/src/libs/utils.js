693aa9235963f7a0b64295bb14588859f0334300
yejineee <43772082+yejineee@users.noreply.github.com>
2020-11-10 14:49:46 +0900

    Feat: 마일스톤 진행률 컴포넌트 추가 (#125)
    * Feat: 마일스톤의 진행률을 보여주는 유틸함수 추가
    
    마일스톤에서 시작 시간부터 끝 시간 사이에서 현재 시간은 몇 %의 진행률인지를
    보여주는 유틸리티 함수를 추가하였다.
    현재시간이 끝시간보다 크다면, 100을 반환한다.
    시작시간이 끝 시간보다 크다면, 에러를 반환한다.
    위 두 경우를 제외하고는 진행률을 % 단위로 반환한다.
    
    * Test: 마일스톤 진행률 테스트 추가
    
    마일스톤 진행률을 보여주는 유틸리티 함수의 테스트를 추가하였다.
    
    * Feat: 마일스톤 진행률 컴포넌트 추가
    
    시작시간과 끝 시간이 주어졌을 때, 현재 시간상 진행률을 보여주는
    컴포넌트를 추가하였다.
    
    Co-authored-by: yejineee <21700431@handong.edu>

--- front/src/libs/utils.js
+++ front/src/libs/utils.js
@@ -14,4 +14,25 @@ export default {
   isEmpty(object) {
     return Object.keys(object).length === 0 && object.constructor === Object;
   },
+  getProgressRate(start, end) {
+    const startDate = moment(start);
+    const endDate = moment(end);
+    const now = moment();
+    if (this.isTimePassedFromNow(end)) {
+      return 100;
+    }
+    if (startDate.diff(endDate) >= 0) {
+      throw new Error('getProgressRate ERROR: 시작 시간이 끝 시간보다 큽니다.');
+    }
+    const rangeFromStartToEnd = endDate.diff(startDate);
+    const rangeFromStartToNow = now.diff(startDate);
+    return Math.floor((rangeFromStartToNow / rangeFromStartToEnd) * 100);
+  },
+  isTimePassedFromNow(date) {
+    const now = moment();
+    if (moment(date).diff(now) >= 0) {
+      return false;
+    }
+    return true;
+  },
 };
--- front/src/libs/utils.test.js
+++ front/src/libs/utils.test.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import utils from './utils';
 
 test('리스트에서 조건에 맞는 엘리먼트의 갯수를 가져온다. ', () => {
@@ -13,4 +14,28 @@ test('리스트에서 조건에 맞는 엘리먼트의 갯수를 가져온다. '
   expect(utils.countFilteredElement(dataList, (data) => !data.isClosed)).toBe(
     1,
   );
+});
+
+describe('현재시간보다 지났는지 확인할 때,', () => {
+  test('현재시간이 기준 시간보다 지났으면 true를 반환한다.', () => {
+    const date = '2020-11-10 13:27:11';
+    expect(utils.isTimePassedFromNow(date)).toBeTruthy();
+  });
+  test('현재시간이 기준 시간을 지나지 않았으면 false를 반환한다.', () => {
+    const date = '2022-11-10 13:28:11';
+    expect(utils.isTimePassedFromNow(date)).toBeFalsy();
+  });
+});
+describe('시작 시간부터 끝 시간을 확인할 때,', () => {
+  test('현재 시간이 시작과 끝 사이에 있으면, [0, 100) 범위의 값을 반환한다.', () => {
+    const createdAt = moment('2020-10-31 09:00:00');
+    const endDate = moment('2020-12-29 21:55:03');
+    expect(utils.getProgressRate(createdAt, endDate)).toBeGreaterThanOrEqual(0);
+    expect(utils.getProgressRate(createdAt, endDate)).toBeLessThan(100);
+  });
+  test('현재 시간이 끝 시간보다 지났다면, 100을 반환한다.', () => {
+    const createdAt = moment('2020-10-31 09:00:00');
+    const endDate = moment('2020-11-01 21:55:03');
+    expect(utils.getProgressRate(createdAt, endDate)).toBe(100);
+  });
 });
