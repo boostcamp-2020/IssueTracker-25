8c541bd259420727255cb45254e8273f535a1195
Changehee Choi <changhee.dev@gmail.com>
2020-11-11 14:23:54 +0900

    Fix: Comment 컴포넌트 레이아웃 틀어짐 수정 및 레이아웃 개선
    새로 merge된 커밋으로 인해 Comment 컴포넌트의 레이아웃이 틀어지는
    문제가 있었고 해당 문제를 수정했습니다.
    수정과정에서 반응형 처리등 레이아웃을 개선시키는 작업을 했습니다.

--- front/src/components/commons/MarkdownViewer.js
+++ front/src/components/commons/MarkdownViewer.js
@@ -5,8 +5,7 @@ import styled from 'styled-components';
 
 const ViewerContainer = styled.div`
   width: 100%;
-  min-height: 230px;
-  padding: 0.25rem 0.25rem 1rem;
+  padding: 0.25rem 1rem 1rem;
 `;
 
 const MarkdownViewer = ({ children }) => {
