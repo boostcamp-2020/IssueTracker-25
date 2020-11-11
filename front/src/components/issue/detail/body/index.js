2198003adb801e734eb1d536f9880a5930c3c95c
Changehee Choi <changhee.dev@gmail.com>
2020-11-11 14:42:43 +0900

    Refactor: 이슈 상세페이지에 SidebarLayout 컴포넌트 적용
    이슈 상세페이지 레이아웃을 SidebarLayout 컴포넌트로 변경해주었습니다.

--- front/src/components/issue/detail/body/index.js
+++ front/src/components/issue/detail/body/index.js
@@ -1,55 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
 import Comment from './Comment';
-
-const IssueDetailBodyContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  .issue-detail-content {
-    width: 100%;
-  }
-
-  .issue-detail-aside {
-    width: 100%;
-  }
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-
-    .issue-detail-content {
-      width: 75%;
-    }
-
-    .issue-detail-aside {
-      width: 25%;
-    }
-  }
-`;
+import SidebarLayout from '../../../commons/SidebarLayout';
 
 const IssueDetailBody = ({ issue }) => {
   return (
-    <IssueDetailBodyContainer>
-      <div className="issue-detail-content">
-        <Comment
-          writer={issue.Author}
-          createdAt={issue.createdAt}
-          contents={issue.contents}
-          isAuthor={issue.isAuthor}
-        />
-        {issue.Comments.map((comment) => {
-          return (
-            <Comment
-              writer={comment.User}
-              createdAt={comment.createdAt}
-              contents={comment.contents}
-              key={`issue-${issue.id}-comment-${comment.id}`}
-            />
-          );
-        })}
-      </div>
-      <div className="issue-detail-aside" />
-    </IssueDetailBodyContainer>
+    <SidebarLayout.BaseLayout>
+      <SidebarLayout.Content>
+        <>
+          <Comment
+            writer={issue.Author}
+            createdAt={issue.createdAt}
+            contents={issue.contents}
+            isAuthor={issue.isAuthor}
+          />
+          {issue.Comments.map((comment) => {
+            return (
+              <Comment
+                writer={comment.User}
+                createdAt={comment.createdAt}
+                contents={comment.contents}
+                key={`issue-${issue.id}-comment-${comment.id}`}
+              />
+            );
+          })}
+        </>
+      </SidebarLayout.Content>
+      <SidebarLayout.Sidebar>sidebar</SidebarLayout.Sidebar>
+    </SidebarLayout.BaseLayout>
   );
 };
 
