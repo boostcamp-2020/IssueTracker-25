249d8db3550b564269ae6455fcf7cae4e7683b7a
rolled-potatoes <keep4404@gmail.com>
2020-11-11 14:07:13 +0900

    Feat : 이슈 디테일 페이지 사이드바 연결

--- front/src/apis/issue.js
+++ front/src/apis/issue.js
@@ -1,10 +1,14 @@
 import axios from './axios';
 
+const ISSUES_URL = '/issues';
 export default {
   getIssues: (page) => {
-    return axios.get(`/issues?page=${page}`);
+    return axios.get(`${ISSUES_URL}?page=${page}`);
   },
   getIssue: (id) => {
-    return axios.get(`/issues/${id}`);
+    return axios.get(`${ISSUES_URL}/${id}`);
+  },
+  postIssue: ({ issue }) => {
+    return axios.post(`${ISSUES_URL}`, issue);
   },
 };
