ae51dda7a3cd08d934bb9082e63400527f499ecd
Changehee Choi <changhee.dev@gmail.com>
2020-11-11 16:14:29 +0900

    Refactor: seeder 코드 수정
    예진님이 작업하신 코드가 충돌이 나서 수정해서 올립니다.
    
    -  랜덤 날짜 생성 함수 utils에 추가
    
    기존에 랜덤 날짜 생성하는 함수를 여러 곳에서 쓰이기 때문에 utils에
    넣도록 한다.
    
    - 마일스톤 종료시간을 랜덤하게 설정
    
    마일스톤의 종료시간을 현재시간부터 랜덤한 시간으로 정하였다.

--- server/src/libs/seeders/milestone.js
+++ server/src/libs/seeders/milestone.js
@@ -1,11 +1,16 @@
 import db from '../../models';
+import utils from '../utils';
 
 const { Milestone: MilestoneModel } = db.sequelize.models;
 
 export async function up() {
   const baseTitle = 'week ';
   const baseDescription = '마일스톤 설명 ';
-  const endDates = ['2020-10-31', null, '2020-12-24'];
+  const endDates = [
+    utils.getRandomDate(new Date(), new Date(2021, 11, 31)),
+    null,
+    utils.getRandomDate(new Date(), new Date(2021, 11, 31)),
+  ];
   const bulkMilestone = endDates.reduce((milestones, endDate, idx) => {
     const milestone = {
       title: `${baseTitle}${idx + 1}`,
--- server/src/libs/utils.js
+++ server/src/libs/utils.js
@@ -2,4 +2,10 @@ export default {
   getValidData(list, idx) {
     return list[idx % list.length];
   },
+  getRandomDate(start, end) {
+    const date = new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime()),
+    );
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  },
 };
