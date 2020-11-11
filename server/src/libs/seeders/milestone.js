0c958d7a63d4859272e9fe2f4f861d6f697dd505
yejineee <21700431@handong.edu>
2020-11-11 11:14:59 +0900

    Fix: milestone endDate 수정
    milestone의 endDate는 createdAt보다 이후여야 하므로,
    createdAt 이후로 수정한다.

--- server/src/libs/seeders/milestone.js
+++ server/src/libs/seeders/milestone.js
@@ -5,7 +5,7 @@ const { Milestone: MilestoneModel } = db.sequelize.models;
 export async function up() {
   const baseTitle = 'week ';
   const baseDescription = '마일스톤 설명 ';
-  const endDates = ['2020-10-31', null, '2020-12-24'];
+  const endDates = ['2020-11-22', null, '2021-05-24'];
   const bulkMilestone = endDates.reduce((milestones, endDate, idx) => {
     const milestone = {
       title: `${baseTitle}${idx + 1}`,
