160c8c8f3482dcf85d2eb19968f497df8bc78cf3
yejineee <21700431@handong.edu>
2020-11-12 18:12:40 +0900

    Style: 이슈 디테일 수정 페이지에서 버튼의 위치를 오른쪽으로 옮김
    버튼의 위치는 디테일 페이지에서 오른쪽에 있어야 하므로,
    justify-content의 flex-end로 설정하여 오른쪽으로 옮겼다.

--- front/src/components/issue/edit/body/index.js
+++ front/src/components/issue/edit/body/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 import MarkdownEditor from '../../../../models/markdown-editor';
 import CustomButton from '../../../commons/buttons/CustomButton';
 
+const ButtonContainer = styled.div`
+  display: flex;
+  margin: 1rem 0;
+  justify-content: flex-end;
+`;
 const IssueEditBody = ({
   initialContents,
   onChange,
@@ -14,15 +20,17 @@ const IssueEditBody = ({
         onChange={(value) => onChange(value)}
         initialContents={initialContents}
       />
-      <CustomButton
-        style={{ color: 'whiteRed' }}
-        handleClick={cancelContentsClickHandler}
-      >
-        Cancel
-      </CustomButton>
-      <CustomButton style={{ color: 'green' }} handleClick={onContentsSave}>
-        Update comment
-      </CustomButton>
+      <ButtonContainer>
+        <CustomButton
+          style={{ color: 'whiteRed' }}
+          handleClick={cancelContentsClickHandler}
+        >
+          Cancel
+        </CustomButton>
+        <CustomButton style={{ color: 'green' }} handleClick={onContentsSave}>
+          Update comment
+        </CustomButton>
+      </ButtonContainer>
     </>
   );
 };
